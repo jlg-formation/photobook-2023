4bc4090b76632d393167806a035018bdea3bb373
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-02-12 14:21:44 +0100

    dev: splash screen. first component.

---
+++ front/screens/SplashScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export const SplashScreen = () => {
+  return (
+    <View>
+      <Text>Splash Screen</Text>
+    </View>
+  );
+};
