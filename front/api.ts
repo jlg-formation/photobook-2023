19194f59473c167b1050285c7fb70a6a19f20cf9
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-02-15 09:31:38 +0100

    dev: added api connect and disconnect

--- front/api.ts
+++ front/api.ts
@@ -1,21 +1,47 @@
+import {domainUrl} from './app.json';
 import {Article, NewArticle} from './interfaces/Article';
 import {User} from './interfaces/User';
 
 const generateId = () => Date.now() + '_' + Math.random() * 1e15;
-const sleep = (delay: number) =>
-  new Promise<void>(resolve => setTimeout(resolve, delay));
 
 class API {
   articles: Article[] = [];
   async connect(login: string, password: string): Promise<User> {
-    console.log('login: ', login);
-    console.log('password: ', password);
-    await sleep(2000);
-    return {displayName: 'Jean-Marc DURAND'};
+    try {
+      console.log('login: ', login);
+      console.log('password: ', password);
+      const response = await fetch(domainUrl + '/api/auth/connect', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({email: login, password}),
+      });
+      if (response.status === 401) {
+        throw new Error('Bad Login');
+      }
+      if (response.status >= 400) {
+        throw new Error();
+      }
+      const user = await response.json();
+      return user;
+    } catch (err) {
+      console.log('err: ', err);
+      if (err instanceof Error) {
+        if (err.message === 'Bad Login') {
+          throw err;
+        }
+      }
+      throw new Error('Technical Error');
+    }
   }
 
   async disconnect() {
-    await sleep(2000);
+    try {
+      await fetch(domainUrl + '/api/auth/disconnect', {method: 'POST'});
+    } catch (err) {
+      console.log('err: ', err);
+    }
   }
 
   async addArticle(newArticle: NewArticle) {
